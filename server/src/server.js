0bc451e44b8070adb50d9c3b47b8d059b8893d3c
hiepcapichi <hiep.tn@capichiapp.com>
2023-12-04 00:06:37 +0700

    change server

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!!");
 });
 
 app.get("/ejs", (req, res) => {
